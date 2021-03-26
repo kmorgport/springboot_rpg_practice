294c2affb3af554da54620c7d7b8074b2b52b595
Kole Morgan <kmorgan0057@gmail.com>
2021-03-26 14:35:19 -0500

    Initial commit

---
+++ src/main/java/com/codeup/springboot_rpg_practice/SpringbootRpgPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.springboot_rpg_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRpgPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRpgPracticeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codeup/springboot_rpg_practice/models/Monster.java
@@ -0,0 +1,4 @@
+package com.codeup.springboot_rpg_practice.models;
+
+public class Monster {
+}
