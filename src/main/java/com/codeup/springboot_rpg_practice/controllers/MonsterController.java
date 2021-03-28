dfbe904866f740916bc5a77525b6d76783ef53d1
Kole Morgan <kmorgan0057@gmail.com>
2021-03-28 17:32:05 -0500

    monster Dao practice

--- src/main/java/com/codeup/springboot_rpg_practice/controllers/MonsterController.java
+++ src/main/java/com/codeup/springboot_rpg_practice/controllers/MonsterController.java
@@ -1,4 +1,15 @@
 package com.codeup.springboot_rpg_practice.controllers;
 
-public class MonsterController {
+import com.codeup.springboot_rpg_practice.repo.MonsterRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
+class MonsterController {
+
+    private final MonsterRepository monsterDao;
+
+    public MonsterController(MonsterRepository monsterDao){
+        this.monsterDao = monsterDao;
+    }
+
 }
--- src/main/java/com/codeup/springboot_rpg_practice/models/Monster.java
+++ src/main/java/com/codeup/springboot_rpg_practice/models/Monster.java
@@ -44,4 +44,6 @@ public class Monster {
 
     public Monster(){}
 
+
+
 }
