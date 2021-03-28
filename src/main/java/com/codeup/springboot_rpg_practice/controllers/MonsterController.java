4754f094d456417e58d1ca96e5bdc7100d38eca2
Kole Morgan <kmorgan0057@gmail.com>
2021-03-28 17:53:52 -0500

    spring boot controller practice

--- src/main/java/com/codeup/springboot_rpg_practice/controllers/MonsterController.java
+++ src/main/java/com/codeup/springboot_rpg_practice/controllers/MonsterController.java
@@ -1,7 +1,14 @@
 package com.codeup.springboot_rpg_practice.controllers;
 
+import com.codeup.springboot_rpg_practice.models.Monster;
 import com.codeup.springboot_rpg_practice.repo.MonsterRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 
 @Controller
 class MonsterController {
@@ -10,6 +17,13 @@ class MonsterController {
 
     public MonsterController(MonsterRepository monsterDao){
         this.monsterDao = monsterDao;
+    }
+
+    @GetMapping("/monster")
+    public String posts(Model model){
+        List<Monster> monsterFromDB = monsterDao.findAll();
+        model.addAttribute("monsters",monsterFromDB);
+        return "monsters/show";
     }
 
 }
